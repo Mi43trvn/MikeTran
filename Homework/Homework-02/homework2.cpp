56439eafbe755683a12f0f18cc859cb0c0318295
Mi43trvn <tranmike.k@gmail.com>
2026-03-18 00:30:47 -0700

    Finish HW2

---
+++ Homework/Homework-02/homework2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string country;
+
+    std::cout << "Where do you live? ";
+    std::getline(std::cin, country);
+
+    std::cout << "I've heard great things about " << country 
+              << ". I'd like to go sometime." << std::endl;
+
+    return 0;
+}
