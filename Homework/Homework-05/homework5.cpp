0067a4e9115e8b3b17188d476d1262ab9e9e5353
Mi43trvn <tranmike.k@gmail.com>
2026-03-18 00:57:23 -0700

    Finish HW5

--- Homework/Homework-05/homework5.cpp
+++ Homework/Homework-05/homework5.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    string input;
+    int vowel_count = 0;
+
+    cout << "Enter a message: ";
+    getline(cin, input);
+
+    // Convert string to char array-like access
+    for (int i = 0; i < input.length(); i++) {
+        char c = input[i];
+
+        // Check vowels (uppercase + lowercase)
+        if (c == 'a' || c == 'A' ||
+            c == 'e' || c == 'E' ||
+            c == 'i' || c == 'I' ||
+            c == 'o' || c == 'O' ||
+            c == 'u' || c == 'U') 
+        {
+            vowel_count++;
+        }
+    }
+
+    cout << "Total vowels: " << vowel_count << endl;
+
+    return 0;
+}
